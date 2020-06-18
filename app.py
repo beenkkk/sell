f789c01caba7edff27fb0e0855e2ebf1cea843ce
beenkkk <hanbeen1019@gmail.com>
2020-06-18 22:19:53 +0900

    1개씩 아이디 선택해서 돌려보던거 for문으로 아이디 없이 적용 중

---
+++ app.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask, render_template, jsonify, request
+app = Flask(__name__)
+from pymongo import MongoClient           # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbsparta                      # 'dbsparta'라는 이름의 db를 만듭니다.
+## HTML을 주는 부분
+@app.route('/')
+def home():
+    return render_template('index.html')
+## API 역할을 하는 부분
+
+@app.route('/order', methods=['POST'])
+def write_order():
+    name_receive = request.form['name_give']
+    price_receive = request.form['price_give']
+    qty_receive = request.form['qty_give']
+    pay_receive = request.form['pay_give']
+    order = {
+        'name': name_receive,
+        'price': price_receive,
+        'qty': qty_receive,
+        'pay': pay_receive
+    }
+    print(order)
+    db.snack.insert_one(order)
+    return jsonify({'result':'success', 'msg': '이 요청은 POST!'})
+
+
+@app.route('/order', methods=['GET'])
+def read_orders():
+    data = list(db.orders.find({}, {'_id': False}))
+    return jsonify({'result':'success', 'msg': '이 요청은 GET!', 'orders': data})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
